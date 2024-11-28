fb7e058a46c3fc4ac937d147a1443edd6587ea5e
Bao44 <tqbao44@gmail.com>
2024-11-28 22:17:45 +0700

    add company

--- src/main/java/vn/edu/iuh/fit/lab_5/frontend/controllers/AdminHomeController.java
+++ src/main/java/vn/edu/iuh/fit/lab_5/frontend/controllers/AdminHomeController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 import vn.edu.iuh.fit.lab_5.backend.enums.SkillType;
 import vn.edu.iuh.fit.lab_5.frontend.models.AdminHomeModel;
+import vn.edu.iuh.fit.lab_5.frontend.models.CompanyModel;
 
 import java.util.Arrays;
 
@@ -15,12 +16,21 @@ import java.util.Arrays;
 public class AdminHomeController {
     @Autowired
     private AdminHomeModel ahm;
+    @Autowired
+    private CompanyModel cpm;
 
     @GetMapping("/skill")
     public ModelAndView directToSkillManagement(ModelAndView mv) {
         mv.addObject("skillTytes", Arrays.stream(SkillType.values()).toList());
         mv.addObject("skills", ahm.getAllSkills());
         mv.setViewName("admin/skill");
+        return mv;
+    }
+
+    @GetMapping("/company")
+    public ModelAndView directToCompanyManagement(ModelAndView mv) {
+        mv.setViewName("admin/company");
+        mv.addObject("companies", cpm.getAllCompanies());
         return mv;
     }
 }
---
+++ src/main/java/vn/edu/iuh/fit/lab_5/frontend/models/CompanyModel.java
@@ -0,0 +1,23 @@
+package vn.edu.iuh.fit.lab_5.frontend.models;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+import vn.edu.iuh.fit.lab_5.backend.models.Company;
+import vn.edu.iuh.fit.lab_5.backend.models.Response;
+
+import java.net.URI;
+import java.util.List;
+
+@Component
+public class CompanyModel {
+    private RestTemplate rt = new RestTemplate();
+    private final String url = "http://localhost:8080/api/company";
+    private ObjectMapper mapper = new ObjectMapper();
+
+    public List<Company> getAllCompanies() {
+        Response response = rt.getForObject(URI.create(url), Response.class);
+        return mapper.convertValue(response.getData(), new TypeReference<List<Company>>() {});
+    }
+}
