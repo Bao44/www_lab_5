1a2b39097bda550bc14c0f4e6b32b9d69dc4c73e
Bao44 <tqbao44@gmail.com>
2024-12-16 14:41:32 +0700

    design html

--- src/main/java/vn/edu/iuh/fit/lab_5/frontend/controllers/HomeController.java
+++ src/main/java/vn/edu/iuh/fit/lab_5/frontend/controllers/HomeController.java
@@ -7,9 +7,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+import vn.edu.iuh.fit.lab_5.backend.models.Candidate;
+import vn.edu.iuh.fit.lab_5.backend.models.CandidateSkill;
+import vn.edu.iuh.fit.lab_5.backend.models.Job;
 import vn.edu.iuh.fit.lab_5.backend.models.JobSkill;
+import vn.edu.iuh.fit.lab_5.frontend.models.JobModel;
 import vn.edu.iuh.fit.lab_5.frontend.models.JobSkillModel;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Controller
@@ -40,6 +45,33 @@ public class HomeController {
     @GetMapping("/forgot-pass")
     public String directToForgotPass() {
         return "forget_pass";
+    }
+
+
+
+    @Autowired
+    private JobSkillModel jobSkillModel;
+    @Autowired
+    private JobModel jm;
+
+    @GetMapping("/")
+    public ModelAndView getIndex(
+            HttpServletRequest request
+    ) {
+        ModelAndView mv = new ModelAndView("index");
+        Candidate target = (Candidate) request.getServletContext().getAttribute("account_login");
+
+        List<JobSkill> jobSkills = jobSkillModel.getAllJobSkills();
+        List<Job> jobs = jm.getAllJobs();
+
+        request.getServletContext().setAttribute("jobSkills", jobSkills);
+        request.getServletContext().setAttribute("jobs", jobs);
+
+        mv.addObject("account_login", target);
+
+        mv.addObject("jobSkills", jobSkills);
+        mv.addObject("jobs", jobs);
+        return mv;
     }
 
 }
